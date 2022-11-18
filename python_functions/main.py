50c4f3ca219c3874e01498ea4cb651f13480057a
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2022-11-18 16:51:30 +0500

    Create main.py

---
+++ python_functions/main.py
@@ -0,0 +1,37 @@
+# function with default argument
+def showEmployee(name, salary=9000):
+    print(f"{name} , salary: {salary} ")
+
+showEmployee("hashir", 15000)
+showEmployee("hashir")
+
+# multiple return values
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+   
+    return addition, subtraction
+res = calculation(40, 10)
+print(res)
+
+# inner function
+
+def outer_function(a,b):
+    
+    def inner_function():
+        return a+b
+    result = inner_function() 
+    return result+5
+answer = outer_function(2,4)    
+print(answer)
+
+# recursive function
+def function(num):
+    if num != 0:
+        return num + function(num -1)
+    else:
+        return 0
+result = function(10)
+print(result)
+    
