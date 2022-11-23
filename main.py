7cb2fc6be17f0f6195512ee636a47b489ebc69b1
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2022-11-23 16:49:20 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,72 @@
+from dict import MENU, resources
 import random
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+is_on = True
+money = 0
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+def sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
 
-#Write your code below this line 👇
-array = [rock,paper,scissors]
-choice = int(input("choose 0 for rock , choose 1 for paper , choose 2 for scissors"))
-if choice == 0:
-  print(f" you chose rock\n {rock}")
-  comp = random.randint(0,len(array)-1)
-  print(f" computer chose \n {array[comp]}")
-  if comp == 2:
-    print("you lost")
-  elif comp == 1:
-    print("you won")
-  else:
-    print("match tied")
-elif choice == 1:
-  print(f"you chose paper\n {paper}")
-  comp = random.randint(0,len(array)-1)
-  print(f"computer chose \n {array[comp]}")
-  if comp == 2:
-    print("you lost")
-  elif comp == 1:
-    print("match tied")
-  else:
-    print("you won")
-else:
-  print(f"you chose scissors:\n {scissors}")
-  comp = random.randint(0,len(array)-1)
-  print(f"computer chose \n {array[comp]}")
-  if comp == 2:
-    print("match tied")
-  elif comp == 1:
-    print("you won")
-  else:
-    print("you lost")
+
+def coins():
+    """ returns the total amount of coins inserted"""
+    print("please enter coins")
+    total =  int(input("how many quarters ?")) * 0.25
+    total += int(input("how many dimes ?")) * 0.1
+    total += int(input("how many nickles ?")) * 0.05
+    total += int(input("how many pennies ?")) * 0.01
+    return total
+
+
+def transaction(money_paid, drink_cost):
+    if money_paid >= drink_cost:
+        change = round(money_paid - drink_cost,2)
+        print(f"here is your {change}$")
+        global money
+        money = drink_cost
+        return True
+    else:
+        print("Sorry that is not enough , money refunded")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+      resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}☕")
+
+
+
+while is_on:
+    choice = input("what would you like espresso/latte/cappuccino ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']} ml  ")
+        print(f"Milk:{resources['milk']} ml")
+        print(f"Coffee:{resources['coffee']} g")
+        print(f"Money: {money}")
+    else:
+        drink = MENU[choice]
+        if sufficient(drink["ingredients"]):
+            payment = coins()
+            if transaction(payment, drink['cost']):
+                 make_coffee(choice, drink["ingredients"])
+
+
+
+
+
+
+
+
+# coins = int(input("Please Insert coins "))
+# quarters =
+
+# pay = quarters + dimes + nickles + pennies
+# if user_coffee == "espresso":
+
