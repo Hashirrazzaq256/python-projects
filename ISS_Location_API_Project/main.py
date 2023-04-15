6c05c7b174b7289618562e60310877701ead55fb
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-04-15 11:51:49 +0500

    Update main.py

--- ISS_Location_API_Project/main.py
+++ ISS_Location_API_Project/main.py
@@ -1,43 +1,45 @@
-# import requests
-# from datetime import datetime
-# MY_LAT = 31.520370
-# MY_LONG = 74.358749
-# parameters = {
-#     "lat":MY_LAT,
-#     "lng":MY_LONG,
-#     "formatted":1,
-# }
-# 
-# response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
-# response.raise_for_status()
-# data = response.json()
-#
-# sunrise = data["results"]["sunrise"].split("T")[0].split(":")[0]
-# sunset = data["results"]["sunset"].split("T")[0].split(":")[0]
-# print(sunset)
-# print(sunrise)
-#
-# time_now = datetime.now()
-
-#
-#
-#
-#
-#
-#
-#
-# # response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# # response.raise_for_status()
-# # data = response.json()
+import time
+import requests
 from astral.sun import sun
 from astral import LocationInfo
-from datetime import datetime, time
+from datetime import datetime
+import smtplib
+MY_LAT = 31.520370
+MY_LONG = 74.358749
+My_EMAIL = "xapastron@gmail.com"
+MY_PASSWORD = "yudlqggkbjtyhcjo"
 
-city = LocationInfo("Lahore", "Pakistan")
+def iss_location():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-s = sun(city.observer, date=datetime.now())
-sunrise = s['sunrise'].time()
-sunset = s['sunset'].time()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-print(sunrise)
-print(sunset)
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 :
+       return True
+    
+
+
+#----------------------------MY Location Sunrise/Sunset Time Check------------------------------------------
+def is_dark_outside():
+
+    city = LocationInfo("Lahore", "Pakistan")
+
+    s = sun(city.observer, date=datetime.now())
+    sunrise = s['sunrise'].time().hour
+    sunset = s['sunset'].time().hour
+    if datetime.now().hour< sunrise or datetime.now().hour > sunset:
+        return True
+while True:
+    time.sleep(60)
+    if iss_location() and is_dark_outside():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=My_EMAIL,password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=My_EMAIL,
+                to_addrs=My_EMAIL,
+                msg="Subject:Look Up\n\n The ISS is Above you in the sky "
+            )
