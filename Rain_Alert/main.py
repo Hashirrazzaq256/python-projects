5ced09a9a88ac66d51b08e864e5ee9a6ab48c8af
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-04-19 23:35:24 +0500

    Update main.py

--- Rain_Alert/main.py
+++ Rain_Alert/main.py
@@ -1,5 +1,22 @@
 import requests
-api_key = "541f022c20f1ac5300c2f663c99d1445"
-response = requests.get(url="https://api.openweathermap.org/data/2.5/weather?lat=31.561920&lon=74.348083&appid=541f022c20f1ac5300c2f663c99d1445")
-data = response.json()
-print(data)
+from twilio.rest import Client
+account_sid = 'AC2c66dece96b3eabae7ae7ca89545556e'
+auth_token = 'd143366c54184a62799ad598e084ee7f'
+
+response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall?lat=31.561920&lon=74.348083&exclude=current,minutely,daily&appid=b2cc51c44c74a937a323c2276f9db551")
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+rain = False
+for hour_data in weather_slice:
+    condition = hour_data["weather"][0]["id"]
+    if int(condition) <700 :
+        rain = True
+if rain:
+
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_='+16205360447',
+        body='It is going to rain today.Remember to bring an umbrella',
+        to='+923174432613'
+    )
+    print(message.status)
