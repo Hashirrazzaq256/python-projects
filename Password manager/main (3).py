746b5cb8b391d6b459032851d0f694ed63667c5a
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-04-25 19:59:01 +0500

    Update main (3).py

--- Password manager/main (3).py
+++ Password manager/main (3).py
@@ -82,6 +82,7 @@ search_button = Button(text="Search", width=13, )
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password )
 generate_password_button.grid(row=3, column=2)
+
 add_button = Button(text="Add", width=36,command= save )
 add_button.grid(row=4, column=1, columnspan=2)
 
