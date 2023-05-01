ab4026b5000a419cdec56a43df756cd7b658db91
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-05-01 15:11:43 +0500

    Update main.py

--- Habit tracker API/main.py
+++ Habit tracker API/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime as dt
 pixela_endpoint = "https://pixe.la/v1/users"
 TOKEN = "hahahahehehe"
 USERNAME = "hashir21"
@@ -24,5 +25,14 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-response = requests.post(url=graph_endpoint, json= graph_config, headers= headers)
+# response = requests.post(url=graph_endpoint, json= graph_config, headers= headers)
+# print(response.text)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+
+today = dt.now()
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many minutes you have coded today?"),
+}
+response = requests.post(url=pixel_creation_endpoint,json= pixel_data,headers= headers)
 print(response.text)
