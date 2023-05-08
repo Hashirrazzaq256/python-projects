6d100c72c9eef7c064127aa44041121d0ab70c84
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-05-08 20:02:12 +0500

    Update main (3).py

--- Web Scraping/main (3).py
+++ Web Scraping/main (3).py
@@ -1,7 +1,33 @@
 from bs4 import BeautifulSoup
-import openai
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+website_content = response.text
+soup = BeautifulSoup(website_content, "html.parser")
+print(soup.find(name="span", class_="titleline"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import lxml
-with open("website.html" ,encoding="utf8") as file:
-    data = file.read()
-soup = BeautifulSoup(data, 'html.parser')
-print(soup.title)
+# with open("website.html" ,encoding="utf8") as file:
+#     data = file.read()
+# soup = BeautifulSoup(data, 'html.parser')
+# anchors = soup.find_all(name="a")
+# # for a in anchors:
+# #     print(a.get("href"))
+# # print(soup.find(name="h3", class_= "heading"))
+# url = soup.select_one(selector="p a")
+# print(url)
