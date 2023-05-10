401ce15554b29357631cd2a37207b669ad802cb3
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-05-10 14:59:28 +0500

    Update main.py

--- Web Scraping/main.py
+++ Web Scraping/main.py
@@ -2,36 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/news")
-website_content = response.text
-soup = BeautifulSoup(website_content, "html.parser")
-print(soup.find(name="span", class_="titleline"))
-first_anchor = soup.find('a')
-first_anchor_text = first_anchor.text
-anchor_tags = soup.find_all('a')
-second_anchor_text = anchor_tags[1].text
+yc_web_page = response.text
 
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
 
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
 
-
-
-
-
-
-
-
-
-
-
-
-
-import lxml
-# with open("website.html" ,encoding="utf8") as file:
-#     data = file.read()
-# soup = BeautifulSoup(data, 'html.parser')
-# anchors = soup.find_all(name="a")
-# # for a in anchors:
-# #     print(a.get("href"))
-# # print(soup.find(name="h3", class_= "heading"))
-# url = soup.select_one(selector="p a")
-# print(url)
+print(article_texts[largest_index])
+print(article_links[largest_index])
